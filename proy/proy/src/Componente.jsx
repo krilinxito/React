8e01f33405c664ae97a9311d4b1913a795c7c5cf
krilinxito <barryallen4207@gmail.com>
2024-11-19 13:40:31 -0400

    Mi Primer Componente

---
+++ proy/proy/src/Componente.jsx
@@ -0,0 +1,11 @@
+import './Componente.css'
+function Componente()
+{
+    return(
+        <div className='Container'>
+            <h1>Hola Mundo</h1>
+        </div>
+    )
+}
+
+export default Componente;
