75c1b915e749c1dfc48482a8eebb6ba04763a8f5
krilinxito <barryallen4207@gmail.com>
2024-11-21 12:22:36 -0400

    jueves

--- jueves/jueves/src/main.jsx
+++ jueves/jueves/src/main.jsx
@@ -2,13 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Componente from './Componente.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Componente/>
-    <br />
-    <Componente/>
   </StrictMode>,
 )
