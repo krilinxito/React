73c33f884f9a526e4a568ac60b3ecc787b31469a
krilinxito <barryallen4207@gmail.com>
2024-11-21 12:53:25 -0400

    creating card

---
+++ jueves/jueves/src/Cartila.jsx
@@ -0,0 +1,17 @@
+import './Cartila.css'
+function Cartila() {
+    return (
+        <div className="container">
+            <h1 className="title">Title</h1>
+            <img
+                src="https://ih1.redbubble.net/image.4646860467.2555/raf,360x360,075,t,fafafa:ca443f4786.jpg"
+                alt="shrek"
+            />
+            <h3 className="description">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil eos nobis esse quae tempore perspiciatis nostrum deserunt itaque numquam vel quaerat pariatur soluta, error doloribus, ratione, neque sapiente iste doloremque?
+            </h3>
+        </div>
+    );
+}
+
+export default Cartila
